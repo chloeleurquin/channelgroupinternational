32382a9848d458c003077dc6aa1a4b50b1b7fe76
chloe75008 <yourGitHub@email.com>
2018-10-02 14:35:00 +0400

    int travel

--- javascript.js
+++ javascript.js
@@ -1,4 +1,15 @@
+$(function(){
+	var includes = $('[data-include]');
+	jQuery.each(includes, function(){
+		var file = $(this).data('include') + '.html';
+		$(this).load(file);
+	});
+});
+
 $(document).ready(function(){
+
+
+	$('#page_effect').fadeIn(500);
 
 	(function($) {
 
